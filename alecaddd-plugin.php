f9a3fc186cc9b2271492fc47798b815c633d11a8
Imon <contact@imonweb.co.uk>
2022-05-07 09:33:32 +0100

    visibility

--- alecaddd-plugin.php
+++ alecaddd-plugin.php
@@ -16,10 +16,30 @@ Text Domain: alecaddd-plugin
 defined( 'ABSPATH' ) or die('Hey, what are you doing here? You silly human!');
  
 class AlecadddPlugin {
+
+  // Public - can be accessed anywhere
+
+  // Protected - can be accessed only within the class itself or extensions of the class
+
+  // Private - can be accessed only within the class itself
+
+  function __construct(){
+    $this->print_stuff();
+  }
+
+  function register(){
+    add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ));
+  }
    
+  protected function create_post_type(){
+    add_action('init', array( $this, 'custom_post_type') );
+  }
+
   function activate() {
     // generate a CPT
+    $this->custom_post_type();
     // flush rewrite rules
+    flush_rewrite_rules();
    }
 
    function deactivate(){
@@ -30,13 +50,37 @@ class AlecadddPlugin {
     // delete CPT
     // delete all plugin data from the DB
    }
+
+   function custom_post_type(){
+     register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
+   }
+
+   function enqueue(){
+    // enqueue all our scripts
+    wp_enqueue_style( 'mypluginstyle', plugins_url( '/assets/mystyles.css', __FILE__ ) );
+    wp_enqueue_script( 'mypluginscript', plugins_url( '/assets/myscript.js', __FILE__ ) );
+   }
+
+   private function print_stuff(){
+     echo 'Test';
+   }
+} // AlecaddPlugin() end
+
+class SecondClass extends AlecadddPlugin {
+  function register_post_type() {
+    $this->create_post_type();
+  }
 }
 
 if( class_exists( 'AlecadddPlugin' ) ){
 
   $alecadddPlugin = new AlecadddPlugin();
+  $alecadddPlugin->register();
    
 }
+
+$secondClass = new SecondClass();
+$secondClass->register_post_type();
 
 // activation
 register_activation_hook( __FILE__, array( $alecadddPlugin, 'activate' ) );
@@ -45,4 +89,4 @@ register_activation_hook( __FILE__, array( $alecadddPlugin, 'activate' ) );
 register_deactivation_hook( __FILE__, array( $alecadddPlugin, 'deactivate' ) );
 
 // uninstall
- 
+// register_uninstall_hook( __FILE__, array( $alecadddPlugin, 'uninstall' ) ); 
