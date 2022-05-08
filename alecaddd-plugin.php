9d061b62621d23b1745c43839372ad2b28ca6371
Imon <contact@imonweb.co.uk>
2022-05-08 11:21:14 +0100

    starter plugin structure

--- alecaddd-plugin.php
+++ alecaddd-plugin.php
@@ -13,14 +13,40 @@ License: GPLv2 or later
 Text Domain: alecaddd-plugin
 */
  
+// If this file is called directly, abort!
 defined( 'ABSPATH' ) or die('Hey, what are you doing here? You silly human!');
  
+// Require once the Composer autoload
 if( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ){
   require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
+// Define CONSTANTS
 define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ));
 define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ));
+define( 'PLUGIN', plugin_basename( __FILE__ ));
+
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+/*  
+* The code that runs during plugin activation 
+*/
+
+function activate_alecaddd_plugin(){
+  Activate::activate();
+}
+
+/*  
+* The code that runs during plugin deactivation 
+*/
+
+function deactivate_alecaddd_plugin(){
+  Deactivate::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_alecaddd_plugin' );
+register_deactivation_hook( __FILE__, 'deactivate_alecaddd_plugin' );
 
 if( class_exists( 'Inc\\Init' ) ) {
   Inc\Init::register_services();
--- inc/Init.php
+++ inc/Init.php
@@ -17,7 +17,8 @@ final class Init {
   public static function get_services(){
     return [
       Pages\Admin::class,
-      Base\Enqueue::class
+      Base\Enqueue::class,
+      Base\settingsLinks::class
     ];
   }
 
