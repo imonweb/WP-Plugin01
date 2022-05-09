78a798d6935f5ea1ebb0d90ca473826d1061eb94
Imon <contact@imonweb.co.uk>
2022-05-09 01:36:57 +0100

    starter plugin structure

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -2,18 +2,35 @@
 
 namespace Inc\Pages;
 
+use \Inc\Base\BaseController;
+use \Inc\Base\SettingsApi;
+
+
 /*  
 * @package AlecadddPlugin
 */
 
-class Admin {
+class Admin extends BaseController {
+
+  public $settings;
 
   // function __construct() { }
 
-  public function register() {
-     add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+  public function __construct(){
+    $this->settings = new SettingsApi();
   }
 
+  public function register() {
+    //  add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+    $pages = [
+      'page_title' => 'Alecaddd Plugin', 'Alecaddd', 'manage_options', 'alecaddd_plugin', array( $this, 'admin_index'), 'dashicons-store', 110;
+    ];
+
+    $this->settings->addPages( 'admin' )->register();
+  }
+
+  /*
   public function add_admin_pages(){
     add_menu_page( 'Alecaddd Plugin', 'Alecaddd', 'manage_options', 'alecaddd_plugin', array( $this, 'admin_index'), 'dashicons-store', 110 );
   }
@@ -21,6 +38,9 @@ class Admin {
   public function admin_index() {
     // require template
       // require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
-      require_once PLUGIN_PATH . 'templates/admin.php';
+      // require_once PLUGIN_PATH . 'templates/admin.php';
+      require_once $this->plugin_path . 'templates/admin.php';
   }
+
+  */
 } // Admin class end here
