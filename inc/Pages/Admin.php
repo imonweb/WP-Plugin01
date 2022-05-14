a0ab2032819df860bedbb5b9bc04c2450898a027
Imon <contact@imonweb.co.uk>
2022-05-14 22:44:48 +0100

    Part14-modular-administration-page

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -1,56 +1,69 @@
 <?php 
-
+/**
+ * @package  AlecadddPlugin
+ */
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
 use \Inc\Api\SettingsApi;
 
-/*  
-* @package AlecadddPlugin
+/**
+* 
 */
+class Admin extends BaseController
+{
+	public $settings;
 
-class Admin extends BaseController {
+	public $pages = array();
 
-  // function __construct() { }
+	public $subpages = array();
 
-  public $settings;
+	public function __construct()
+	{
+		$this->settings = new SettingsApi();
 
-  public $pages = array();
+		$this->pages = array(
+			array(
+				'page_title' => 'Alecaddd Plugin', 
+				'menu_title' => 'Alecaddd', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_plugin', 
+				'callback' => function() { echo '<h1>Alecaddd Plugin</h1>'; }, 
+				'icon_url' => 'dashicons-store', 
+				'position' => 110
+			)
+		);
 
-  public function __construct(){
+		$this->subpages = array(
+			array(
+				'parent_slug' => 'alecaddd_plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_cpt', 
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'alecaddd_plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_taxonomies', 
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'alecaddd_plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'alecaddd_widgets', 
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+			)
+		);
+	}
 
-    $this->settings = new SettingsApi();
- 
-  }
-
-  public function register() {
-    //  add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
-
-    $pages =  array(
-    
-        array(
-          'page_title' => 'Alecaddd Plugin', 
-          'menu_title' => 'Alecaddd', 
-          'capability' => 'manage_options', 
-          'menu_slug' => 'alecaddd_plugin', 
-          'callback' => function(){ echo '<h1> Plugin </h1>'; }, 
-          'icon_url' => 'dashicons-store', 
-          'position' => 110
-        )
-    ); 
-
-    $this->settings->addPages( $pages )->register();
-    // $this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->register();
-  }
-
-  // public function add_admin_pages(){
-  //   add_menu_page( 'Alecaddd Plugin', 'Alecaddd', 'manage_options', 'alecaddd_plugin', array( $this, 'admin_index'), 'dashicons-store', 110 );
-  // }
-
-  // public function admin_index() {
-    // require template
-     // require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';
-      // require_once PLUGIN_PATH . 'templates/admin.php';
-      // require_once $this->plugin_path . 'templates/admin.php';
-  // }
-} // Admin class end here
+	public function register() 
+	{
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+	}
+}
